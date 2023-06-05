197f33f9e3254e71bc19984813c836e0393ce497
asart11 <sora257x@gmail.com>
2023-06-05 15:05:01 +0900

    micromodal

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -8,6 +8,12 @@ window._ = require('lodash');
 
 window.axios = require('axios');
 
+import MicroModal from 'micromodal';
+
+MicroModal.init({
+    disableScroll: true
+});
+
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 /**
