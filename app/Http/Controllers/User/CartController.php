44b89cc42f716ab144c1a395042be622389dea2e
asart11 <sora257x@gmail.com>
2023-06-09 14:50:07 +0900

    オーナー向けメール送信

--- app/Http/Controllers/User/CartController.php
+++ app/Http/Controllers/User/CartController.php
@@ -10,6 +10,8 @@ use App\Models\Stock;
 use Illuminate\Support\Facades\Auth;
 use App\Services\CartService;
 use App\Jobs\SendThanksMail;
+use App\Jobs\SendOrderedMail;
+
 
 
 class CartController extends Controller
@@ -57,15 +59,7 @@ class CartController extends Controller
 
     public function checkout()
     {
-        ////
-        $items = Cart::where('user_id',Auth::id())->get();
-        $products = CartService::getItemsInCart($items);
-        $user = User::findOrFail(Auth::id());
-
-
-        SendThanksMail::dispatch($products,$user);
-        dd('ユーザーめーる送信テスト');
-        /////
+       
 
 
         $user = User::findOrFail(Auth::id());
@@ -123,6 +117,18 @@ class CartController extends Controller
 
     public function success()
     {
+         ////
+         $items = Cart::where('user_id',Auth::id())->get();
+         $products = CartService::getItemsInCart($items);
+         $user = User::findOrFail(Auth::id());
+ 
+ 
+         SendThanksMail::dispatch($products,$user);
+         foreach($products as $product){
+             SendOrderedMail::dispatch($product, $user);
+         }
+        //  dd('ユーザーめーる送信テスト');
+         /////
         Cart::where('user_id',Auth::id())->delete();
 
         return redirect()->route('user.items.index');
